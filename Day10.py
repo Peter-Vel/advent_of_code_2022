e40016c55bfe86fe6d7340649cdeb0647b9eff0e
peter.vel <peterdvel@gmail.com>
2022-12-15 15:57:15 -0800

    Completed day 10

--- Day10.py
+++ Day10.py
@@ -1,24 +1,23 @@
 # %% [markdown]
-# # Part One
+# # Part One & Two
 
 # %%
 file = open('./Input/Day10_Input.txt', 'r')
 
 contents = file.read()
-cmds = contents.split("\n")
+cmds = contents.split('\n')
+crt = [[],[],[],[],[],[]]
+
+for r in crt:
+    pxl_ct = 1
+
+    while pxl_ct <= 40:
+        r.append('.')
+
+        pxl_ct = pxl_ct + 1
 
 # %%
-cmd_ct = len(cmds)
-cmd_ix = 0
-cycle_ct = 0
-cycle_target = 0
-addx_values = [1]
-cycle_wait = False
-signal_intervals = [20, 60, 100, 140, 180, 220]
-signal_strengths = []
-
-# %%
-def process_interval(ct, intervals, addx, strengths):
+def process_strength_interval(ct, intervals, addx, strengths):
     if ct in intervals:
         signal_strength = 0
 
@@ -29,35 +28,78 @@ def process_interval(ct, intervals, addx, strengths):
 
     return strengths
 
-# %%
-while True:
-    cycle_ct = cycle_ct + 1
-
-    if cycle_ct == cycle_target:
-        cycle_wait = False
-
-    if cmds[cmd_ix] == 'noop':
-        signal_strengths = process_interval(cycle_ct, signal_intervals, addx_values, signal_strengths)
-
-        cmd_ix = cmd_ix + 1
+def move_pixel(ct, row_ix, col_ix, intervals):
+    if ct in intervals:
+        # Move to next row
+        row_ix = row_ix + 1
+        col_ix = 0
     else:
-        signal_strengths = process_interval(cycle_ct, signal_intervals, addx_values, signal_strengths)
+        # Move to the right within current row
+        col_ix = col_ix + 1
 
-        if cycle_ct == 220:
-            break
+    return row_ix, col_ix
 
-        if cycle_target < cycle_ct:
-            v = int(cmds[cmd_ix].split(' ')[1])
-            cycle_target = cycle_ct + 1
-            cycle_wait = True
+def start_circuit(cmds, crt):
+    cmd_ct = len(cmds)
+    cmd_ix = 0
+    cycle_ct = 0
+    cycle_target = 0
+    addx_values = [1]
+    cycle_wait = False
+    signal_intervals = [20, 60, 100, 140, 180, 220]
+    signal_strengths = []
+    crt_intervals = [40, 80, 120, 160, 200, 240]
+    crt_row_ix = 0
+    crt_col_ix = 0
+    sprite_cntr = 1
+
+    while True:
+        cycle_ct = cycle_ct + 1
+
+        left_pxl = sprite_cntr - 1
+        right_pxl = sprite_cntr + 1
+        sprite = [left_pxl, sprite_cntr, right_pxl]
+
+        if crt_col_ix in sprite:
+            # Light up that pixel!
+            crt[crt_row_ix][crt_col_ix] = '#'
+
+        if cycle_ct == cycle_target:
+            cycle_wait = False
+
+        if cmds[cmd_ix] == 'noop':
+            signal_strengths = process_strength_interval(cycle_ct, signal_intervals, addx_values, signal_strengths)
+
+            cmd_ix = cmd_ix + 1
         else:
-            if cycle_wait == False:
-                # Finish addx exec
-                addx_values.append(v)
-                cmd_ix = cmd_ix + 1
+            signal_strengths = process_strength_interval(cycle_ct, signal_intervals, addx_values, signal_strengths)
 
-sig_strength_sum = sum(signal_strengths)
+            if cycle_target < cycle_ct:
+                v = int(cmds[cmd_ix].split(' ')[1])
+                cycle_target = cycle_ct + 1
+                cycle_wait = True
+            else:
+                if cycle_wait == False:
+                    # Finish addx exec
+                    addx_values.append(v)
+                    sprite_cntr = sprite_cntr + v
+                    cmd_ix = cmd_ix + 1
 
-print(f"Answer (part one) - The sum of the signal strengths is {sig_strength_sum}")
+        if cycle_ct == 240:
+            sig_strength_sum = sum(signal_strengths)
+
+            return sig_strength_sum, crt
+        else:
+            crt_row_ix, crt_col_ix = move_pixel(cycle_ct, crt_row_ix, crt_col_ix, crt_intervals)
+
+# %%
+sig_strength_sum, crt = start_circuit(cmds, crt)
+
+print(f"Answer (part one) - The sum of the signal strengths is {sig_strength_sum}\n")
+
+print(f"Answer (part two) - 8 capital letters shown below:")
+
+for r in crt:
+    print(r)
 
 
